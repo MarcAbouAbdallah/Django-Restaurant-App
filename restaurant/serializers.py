5fb6488fcbd03f53d442e0e94602142d2b94bc62
MarcAbouAbdallah <marcabouabdallah112304@gmail.com>
2024-07-24 15:10:50 -0400

    set up the basics of two APIs

---
+++ restaurant/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from .models import Menu, Booking
+from django.contrib.auth.models import User
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['id', 'title', 'price', 'inventory']
+        read_only = ['id']
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ['id', 'name', 'no_of_guests', 'booking_date']
+        read_only = ['id']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
 
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import viewsets
+#from rest_framework.permissions import IsAuthenticated
+
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    #permission_classes = [IsAuthenticated]
+
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    #permission_classes = [IsAuthenticated]
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    #permission_classes = [IsAuthenticated]
+
+
 def index(request):
     return render(request, 'index.html', {})
